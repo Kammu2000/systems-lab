9f75b166062e95ff5fdf878bcc5cce983cf06319
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-05-25 14:52:03 +0530

    [SYSTEMS-LAB] feat: Implemented custom unique ptr

---
+++ smart-pointers/unique-ptr/main.cpp
@@ -0,0 +1,25 @@
+#include "unique_ptr.hpp"
+#include <iostream>
+#include <utility>
+
+struct Person
+{
+    std::string name;
+
+    void greet() const { std::cout << "Hello " << name << "\n"; }
+};
+
+int main()
+{
+    UniquePtr<int> ptr(new int(5));
+    std::cout << *ptr << "\n";
+
+    UniquePtr<int> other_ptr = std::move(ptr);
+    std::cout << other_ptr.get() << "\n";
+    std::cout << ptr.get() << "\n";
+
+    UniquePtr<Person> person_ptr(new Person{"deepanshu"});
+    person_ptr->greet();
+
+    return 0;
+}
---
+++ smart-pointers/unique-ptr/unique_ptr.hpp
@@ -0,0 +1,49 @@
+#include <utility>
+
+template <typename T>
+class UniquePtr
+{
+  public:
+    explicit UniquePtr(T* other_ptr = nullptr) : ptr(other_ptr) {};
+    UniquePtr(const UniquePtr&) = delete;
+    UniquePtr& operator=(const UniquePtr&) = delete;
+    UniquePtr(UniquePtr&& other_ptr) noexcept : ptr(other_ptr.ptr) { other_ptr.ptr = nullptr; };
+    UniquePtr& operator=(UniquePtr&& other_ptr) noexcept
+    {
+        if (ptr != other_ptr.ptr)
+        {
+            delete ptr;
+            ptr = other_ptr.ptr;
+            other_ptr.ptr = nullptr;
+        }
+
+        return *this;
+    };
+    ~UniquePtr()
+    {
+        delete ptr;
+        ptr = nullptr;
+    };
+
+    T* get() const { return ptr; }
+    T* operator->() const { return ptr; }
+    T& operator*() const { return *ptr; }
+
+    T* release()
+    {
+        T* old = ptr;
+        ptr = nullptr;
+        return old;
+    }
+
+    void reset(T* other_ptr = nullptr)
+    {
+        delete ptr;
+        ptr = other_ptr;
+    }
+
+    void swap(UniquePtr& other_ptr) noexcept { std::swap(ptr, other_ptr.ptr); }
+
+  private:
+    T* ptr;
+};
